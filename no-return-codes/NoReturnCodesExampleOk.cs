c20d0110e1bfb3f6a862af25740222af1577f6ce
matiaspw <matias.paz.w@gmail.com>
2018-10-04 00:53:53 -0300

    Update NoReturnCodesExampleOk.cs

--- no-return-codes/NoReturnCodesExampleOk.cs
+++ no-return-codes/NoReturnCodesExampleOk.cs
@@ -1,65 +1,64 @@
 ﻿using System;
 namespace exceptions.noreturncodes
 {
-	public class NoReturnCodesExampleOk
+    public class NoReturnCodesExampleOk
     {
-        public static void DoSomething()
+        public void DoSomething()
         {
 
-			var connection = new ResourceConnectionOk();
+	    var connection = new ResourceConnectionOk();
             var connectionString = "example";
 
             try
-			{
-				connection.Open(connectionString);
-				var result = connection.SendQuery("SELECT * FROM People");
+	    {
+		connection.Open(connectionString);
+		var result = connection.ExecuteQuery("SELECT * FROM People");
 
                 connection.Close();
 
-				UseResult(result);
-			}
-			catch (Exception ex)
-			{
-				HandleConnectionError(ex);
-			}
+		UseResult(result);
+	    }
+	    catch (Exception ex)
+	    {
+		HandleConnectionError(ex);
+	    }
         }
 
-		static void UseResult(object result)
-		{
-			//Do something
-		}
+	void UseResult(object result)
+	{
+		//Do something
+	}
 
-		static void HandleConnectionError(Exception ex)
+	void HandleConnectionError(Exception ex)
         {
-			//Do something
-		}
+		//Do something
+	}
     }
 
     public class ResourceConnectionOk
     {
         public StatusConnectionOk Open(string connectionString)
         {
-
             if (ConnectionOk(connectionString))
             {
                 return StatusConnectionOk.Open;
             }
 
-			throw new ApplicationException("Connection Error.");
+	    throw new ApplicationException("Connection Error.");
         }
 
-		public void Close()
-		{
-			//Do something
-		}
+	public void Close()
+	{
+	  //Do something
+	}
 
-		public object SendQuery(string query)
-		{
-			//Execute query.
-			return new Object();
-		}
+	public object ExecuteQuery(string query)
+	{
+		//Execute query.
+		return new Object();
+	}
 
-		bool ConnectionOk(string connectionString)
+	bool ConnectionOk(string connectionString)
         {
             return true;
         }
